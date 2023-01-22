57b0fc788325b44d819e1b0bca66f21b204fee9c
sachin jeph <sachinjeph@sachins-MacBook-Air.local>
2023-01-22 16:55:58 +0530

    RuntimeFPG methods implemented

--- src/LPC/FilePathManager.js
+++ src/LPC/FilePathManager.js
@@ -28,11 +28,14 @@ class SLFilePathManager{
         this.filePaths = [];
         this.fpgToFilesMapping = {};
         this.configFile =  {}
+        this.fpgSet = new Set();
+        //runtimeFPG represents current files user is viewing/using
+        this.runtimeFPG = {};//dictionary of dictionary {'fpgName': {'dirName':set of files in that directory which are needed}}
         console.log("** SLFilePathManager initialised")
     }
 
     /**
-    * Loads the file paths from app.config
+    *PUBLIC: Loads the file paths from app.config
     */
     async initialise(){
         let success = await this.#loadConfig();
@@ -40,6 +43,9 @@ class SLFilePathManager{
             this.#populateFilePaths();
             console.log("** FilePaths added from app.config")
         }
+
+        //LATER
+        //this.runtimeFPG = fetch state from db later, do a promise.all async await 
         
     }
 
@@ -82,17 +88,91 @@ class SLFilePathManager{
     #createSingleDirectorySingleFilePath(directoryPath,fileNameRegex,fpgArr){
         //Adding defaultFPGGroups
         fpgArr = fpgArr.concat(defaultFPGGroups);
+        //Add to FPGSet
+        this.fpgSet = new Set(fpgArr.concat([...this.fpgSet]));
+        console.log("FPGSet: ",this.fpgSet);
         const filePath = new SLFilePath(directoryPath,fileNameRegex,fpgArr);
         this.filePaths.push(filePath)
         console.log("File path added: ",filePath);
     }
     
+    /** RUNTIME FPG  */
 
     /**
-     * Fetches all the files belonging to the particular fpgGroup async and add them to the fpgToFilesMapping cache
+    * PUBLIC: returns runTimeFPG instance, which is a dictionary of dictionary {'fpgName': {'dirName':set of files in that directory which are needed}}
+    * This can also be treated as the recents FPG
+    */
+    getRuntimeFPG(){
+        return this.runtimeFPG;
+    }
+    /**
+     * PUBLIC: Adds all the files belonging to the particular fpgGroup async in runtimFPG
+     */
+    async addFPGtoRuntimeFPG(fpgName){//we need to initially load it from memory
+        let list = await this.filePathManager.fetchFilesBelongingToFPG(fpgName);
+        this.runtimeFPG[fpgName]=list;
+        //matchingFiles is dict where key is the directory and value is set containing fileNames
+        console.log("**Added FPG to runtimeFPG")
+        this.#saveRuntimeFPGState();
+    }
+
+    /**
+     * PUBLIC: remove FPG from runtimFPG
+     */
+    removeFPGtoRuntimeFPG(fpgName){
+        if (this.runtimeFPG.hasOwnProperty(fpgName)) {
+            delete this.runtimeFPG[fpgName];
+            console.log("**Removed FPG from runtimeFPG");
+            this.#saveRuntimeFPGState();
+        } else {
+            console.log(`FPG with name ${fpgName} not found in runtimeFPG`);
+        }
+    }
+
+    /**
+     * PUBLIC: Adds a the files to runtimFPG
+     */
+    addFilePathtoRuntimeFPG(directoryPath, fileName, fpgName){
+        //matchingFiles is dict where key is the directory and value is set containing fileNames
+        if (!this.runtimeFPG[fpgName]) {
+            this.runtimeFPG[fpgName] = { matchingFiles: {} }
+        }
+        if (!this.runtimeFPG[fpgName].matchingFiles[directoryPath]) {
+            this.runtimeFPG[fpgName].matchingFiles[directoryPath] = new Set();
+        }
+        this.runtimeFPG[fpgName].matchingFiles[directoryPath].add(fileName);
+        this.#saveRuntimeFPGState();
+        console.log("**Added single file to runtimeFPG")
+    }
+
+    /**
+     * PUBLIC: Adds a the files to runtimFPG
+     */
+    removeFilePathtoRuntimeFPG(directoryPath, fileName, fpgName){
+        if (this.runtimeFPG[fpgName] && this.runtimeFPG[fpgName].matchingFiles[directoryPath]) {
+            this.runtimeFPG[fpgName].matchingFiles[directoryPath].delete(fileName);
+            console.log("**Removed file from runtimeFPG");
+            this.#saveRuntimeFPGState();
+        } else {
+            console.log(`Error: /removeFilePathtoRuntimeFPG. File with name ${fileName} in directory ${directoryPath} not found in FPG ${fpgName}`);
+        }
+    }
+
+    /**
+     * Saves runtimeFPG state in db
+     */
+    #saveRuntimeFPGState(){
+        //LATER
+        console.log("**runtimeFPG state saving NOT IMPLEMENTED")
+    }
+    
+
+    /** ****************** */
+
+    /**
+     * PUBLIC: Fetches all the files belonging to the particular fpgGroup async and add them to the fpgToFilesMapping cache
      */
     async fetchFilesBelongingToFPG(fpgName){
-       
         if(this.fpgToFilesMapping[fpgName]){
             console.log("Providing FPG files avaialable in cache")
             return this.fpgToFilesMapping[fpgName]
@@ -124,7 +204,7 @@ class SLFilePathManager{
           }));
 
         this.fpgToFilesMapping[fpgName]=matchingFiles;
-        console.log(`Returning Matching files for FPG '${fpgName}' and saved to cache: ${matchingFiles}`); 
+        console.log(`Returning Matching files for FPG '${fpgName}' and saved to cache`); 
         return matchingFiles;
     }
 
@@ -143,6 +223,12 @@ class SLFilePathManager{
         } 
     };
 
+    /**
+     *PUBLIC: Returns a list of all FPGs 
+    */
+    getFPGList(){
+        return this.fpgSet;
+    }
 
     resetFPGFileListCache(){
         this.fpgToFilesMapping = {};
